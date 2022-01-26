85beb62621712b1dfd0db1cadd3d026497699b32
Alexandru Visan <alexandruvisan1908@gmail.com>
2022-01-26 11:10:16 +0200

    changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-document-import-in-page */
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { Helmet } from 'react-helmet';
+import { useRef } from 'react';
 // Via https://github.com/vercel/next.js/blob/canary/examples/with-react-helmet/pages/_document.js
 
 export default class MyDocument extends Document {
